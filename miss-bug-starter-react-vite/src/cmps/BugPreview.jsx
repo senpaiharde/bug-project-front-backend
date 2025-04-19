7d9241419825104d962e8c84abc8885b6054bacc
senpaiharde <xxslavan1@gmail.com>
2025-04-19 17:34:53 +0300

    full working routes ui fix, next step to add hardcoded admin + option to see owner of the bug and option tthat only the owner or admin can edit or delete their bug

--- miss-bug-starter-react-vite/src/cmps/BugPreview.jsx
+++ miss-bug-starter-react-vite/src/cmps/BugPreview.jsx
@@ -3,8 +3,8 @@
 export function BugPreview({ bug }) {
 
     return <article >
-        <h4 className="bug-details-p">{bug.title}</h4>
-        <h1>🐛</h1>
+        <h4 className="bug-details-p">{bug.title || 'No Title'}</h4>
+        <h1>🐛🐛</h1>
         <p className="bug-details-p"> Severity: <span>{bug.severity}</span></p>
     </article>
 }
