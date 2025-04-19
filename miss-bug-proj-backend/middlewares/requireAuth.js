83b0927e9c1836b8c4e7dbeec79a223eb5d0e8a9
senpaiharde <xxslavan1@gmail.com>
2025-04-20 00:02:08 +0300

    setting up docker

--- miss-bug-proj-backend/middlewares/requireAuth.js
+++ miss-bug-proj-backend/middlewares/requireAuth.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 
-const JWT_SECRET = 'missBugSecretKey'
+const JWT_SECRET = process.env.JWT_SECRET || 'fallbackSecret'
+
 
 export function requireAuth(req, res, next) {
     const token = req.headers.authorization?.split(' ')[1]
