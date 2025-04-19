8efcf3c7a1712e5e979a9e84ec644af4109da49e
senpaiharde <xxslavan1@gmail.com>
2025-04-19 16:48:37 +0300

    create better files structure plans osolate navigation like tacker folder will live under dashboard that will help me to scale the site for more sections and maintaince

--- miss-bug-starter-react-vite/src/RootCmp.jsx
+++ miss-bug-starter-react-vite/src/RootCmp.jsx
@@ -1,8 +1,8 @@
 import { AppHeader } from './cmps/AppHeader.jsx';
 import { AppFooter } from './cmps/AppFooter.jsx';
 import { Home } from './pages/Home.jsx';
-import { BugIndex } from './pages/BugIndex.jsx';
-import { BugDetails } from './pages/BugDetails.jsx';
+import {Bugindex} from './pages/tracker/BugIndex.jsx'
+
 import { AboutUs } from './pages/AboutUs.jsx';
 import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import { Login } from './cmps/Login.jsx';
@@ -10,6 +10,7 @@ import { Signup } from './cmps/Signup.jsx';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { logout } from './services/auth.service.js';
+import { BugDetails } from './pages/tracker/BugDetails.jsx';
 
 export function App() {
   const [user, setUser] = useState(null);
@@ -60,7 +61,7 @@ export function App() {
       <main className="container">
         <Routes>
           <Route path="/" element={<Home user={user} setUser={setUser} />} />
-          <Route path="/bug" element={<BugIndex />} />
+          <Route path="/bug" element={<Bugindex />} />
           <Route path="/bug/:bugId" element={<BugDetails user={user} />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/login" element={<Login setUser={setUser} />} />
