1fbc9e1025e061967a25f3f415c09b318da5f43f
senpaiharde <xxslavan1@gmail.com>
2025-04-21 15:57:23 +0300

    added area for sgin in same style as login to to fix bug on navigation and signup button missing

--- miss-bug-starter-react-vite/src/cmps/Signup.jsx
+++ miss-bug-starter-react-vite/src/cmps/Signup.jsx
@@ -26,79 +26,32 @@ export function Signup({ setUser }) {
   }
 
   return (
-    import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import { login } from '../services/auth.service';
-import { Link } from 'react-router-dom';
+    <div className="FormMain">
+      <form className="box" onSubmit={onSubmit}>
+        <div className="login">
+          <div className="loginBx">
+            <h2>
+              <i className="fa-solid fa-right-to-bracket"></i>
+              Sign up
+              <i className="fa-solid fa-heart"></i>
+            </h2>
 
-export function Login({ setUser }) {
-  const [credentials, setCredentials] = useState({ email: '', password: '' });
-
-  const navigate = useNavigate();
-
-  function handleChange(ev) {
-    const { name, value } = ev.target;
-    setCredentials((prev) => ({ ...prev, [name]: value }));
-  }
-
-  async function onSubmit(ev) {
-    ev.preventDefault();
-    try {
-      const user = await login(credentials);
-      setUser(user);
-      alert(`Welcome ${user.fullname}`);
-
-      navigate('/');
-    } catch (err) {
-      console.error('Login failed:', err.response?.data || err.message);
-      alert('login failed');
-    }
-  }
-
-  return (<div className='FormMain'>
-    <form className="box"  onSubmit={onSubmit}>
-      <div className="login">
-        <div className="loginBx">
-          <h2>
-            <i className="fa-solid fa-right-to-bracket"></i>
-            Login
-            <i className="fa-solid fa-heart"></i>
-          </h2>
-          <input
-            type="text"
-            placeholder="Email"
-            autoComplete="Email"
-            onChange={handleChange}
-            name="email"
-          />
-          <input
-          style={{ marginLeft: '75px' }}
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        <input
-          style={{ marginLeft: '15px' }}
-          name="password"
-          placeholder="Password"
-          type="password"
-          onChange={handleChange}
-        />
-        <input
-          style={{ marginLeft: '15px' }}
-          name="fullname"
-          placeholder="Full Name"
-          onChange={handleChange}
-        />
-          <div className="group">
-            
-            <a href="/login">Sign In</a>
+            <input name="email" placeholder="Email" onChange={handleChange} />
+            <input name="password" placeholder="Password" type="password" onChange={handleChange} />
+            <input
+              autoComplete="full-name"
+              name="fullname"
+              placeholder="Full Name"
+              onChange={handleChange}
+            />
+            <input type="submit" value="Sign in" />
+            <div className="group">
+              <a href="#">Back to Home</a>
+              <a href="/login">Sign In</a>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
     </div>
-  );
-}
   );
 }
