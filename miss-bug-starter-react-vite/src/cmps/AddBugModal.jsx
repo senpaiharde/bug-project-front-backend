0842a8cb3969ce121448efa4bb3d013f89920742
senpaiharde <xxslavan1@gmail.com>
2025-04-22 14:29:25 +0300

    added varibables for while prject then will restuctre + full ready ui for input with handler + input now will add scss andimplament to the main input

--- miss-bug-starter-react-vite/src/cmps/AddBugModal.jsx
+++ miss-bug-starter-react-vite/src/cmps/AddBugModal.jsx
@@ -12,4 +12,58 @@ export default async function AddBugModal({isOpen,onClose,onSubmit}) {
         if(isOpen) document.addEventListener('keydown',handleKey)
             return()=> document.addEventListener('keydown',handleKey) 
     },[isOpen,onClose])
+
+
+
+    if(!isOpen) return null;
+
+    function handleOverlayClick() {
+        onClose();
+    }
+
+    function  hanldeContentClick(e) {
+        e.stopPropagation();
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const fm = new FormData(e.target);
+        onsubmit({
+            title:fm.get('Bug title?') || 'Untitled Bug',
+            severity:+fm.get('Bug severity?') || 1,
+            description: fm.get('Bug description?') || 'No description',
+
+        });
+    }
+
+    return(
+        <div className="ModalOverlay" onClick={hanldeContentClick}>
+            <div className="modalConttent" 
+            ref={contentRef} 
+            onClick={hanldeContentClick}>
+                <h2>Add New Bug</h2>
+                <form className="ModalForm" onSubmit={handleSubmit}>
+                    <input name="title" placeholder="Title" required/>
+                    <input 
+                    name="severity" 
+                    type="number"
+                    min='1'
+                    max='5'
+                    placeholder="Severity" 
+                    required
+                    /> 
+                    <textarea name="description" placeholder="Description" required/>
+                    <div className="modal-actions">
+                        <button type="button" onClick={onClose}>
+                            Cancel
+                        </button>
+                        <button type="submit">Save</button>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    )
+
+
 }
