d4776ef1cfc77a0f4ca46775d71f4b7a2caa1b20
senpaiharde <xxslavan1@gmail.com>
2025-04-22 15:26:36 +0300

    update

--- miss-bug-starter-react-vite/src/cmps/AddBugModal.jsx
+++ miss-bug-starter-react-vite/src/cmps/AddBugModal.jsx
@@ -1,75 +1,59 @@
-import PropTypes from "prop-types";
-import { useEffect, useRef } from "react";
+import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
 
+export default function AddBugModal({ isOpen, onClose, onSubmit }) {
+  const contentRef = useRef();
 
-export default  function AddBugModal({isOpen,onClose,onSubmit}) {
-    const contentRef = useRef()
-
-    useEffect(() => {
-        function handleKey(e) {
-            if(e.key === 'Escape') onClose();
-        }
-        if(isOpen) document.addEventListener('keydown',handleKey)
-            return()=> document.addEventListener('keydown',handleKey) 
-    },[isOpen,onClose])
-
-
-
-    if(!isOpen) return null;
-
-    function handleOverlayClick() {
-        onClose();
+  useEffect(() => {
+    function handleKey(e) {
+      if (e.key === 'Escape') onClose();
     }
+    if (isOpen) document.addEventListener('keydown', handleKey);
+    return () => document.addEventListener('keydown', handleKey);
+  }, [isOpen, onClose]);
 
-    function  hanldeContentClick(e) {
-        e.stopPropagation();
-    }
+  if (!isOpen) return null;
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        const fm = new FormData(e.target);
-        onSubmit({
-            title:fm.get('Bug title?') || 'Untitled Bug',
-            severity:+fm.get('Bug severity?') || 1,
-            description: fm.get('Bug description?') || 'No description',
+  function handleOverlayClick() {
+    onClose();
+  }
 
-        });
-    }
+  function hanldeContentClick(e) {
+    e.stopPropagation();
+  }
 
-    return(
-        <div className="ModalOverlay" onClick={handleOverlayClick}>
-            <div className="modalConttent" 
-            ref={contentRef} 
-            onClick={hanldeContentClick}>
-                <h2>Add New Bug</h2>
-                <form className="ModalForm" onSubmit={handleSubmit}>
-                    <input name="title" placeholder="Title" required/>
-                    <input 
-                    name="severity" 
-                    type="number"
-                    min='1'
-                    max='5'
-                    placeholder="Severity" 
-                    required
-                    /> 
-                    <textarea name="description" placeholder="Description" required/>
-                    <div className="modalActions">
-                        <button type="button" onClick={onClose}>
-                            Cancel
-                        </button>
-                        <button type="submit">Save</button>
-                    </div>
-                    
-                </form>
-            </div>
-        </div>
-    )
+  function handleSubmit(e) {
+    e.preventDefault();
+    const fm = new FormData(e.target);
+    onSubmit({
+      title: fm.get('title'),
+      severity: +fm.get('severity'),
+      description: fm.get('description'),
+    });
+  }
 
-
+  return (
+    <div className="ModalOverlay" onClick={handleOverlayClick}>
+      <div className="modalConttent" ref={contentRef} onClick={hanldeContentClick}>
+        <h2>Add New Bug</h2>
+        <form className="ModalForm" onSubmit={handleSubmit}>
+          <input name="title" placeholder="Title" required />
+          <input name="severity" type="number" min="1" max="5" placeholder="Severity" required />
+          <textarea name="description" placeholder="Description" required />
+          <div className="modalActions">
+            <button type="button" onClick={onClose}>
+              Cancel
+            </button>
+            <button type="submit">Save</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-AddBugModal.PropTypes = {
-    isOpen : PropTypes.bool.isRequired,
-    onClose:  PropTypes.func.isRequired,
-    onSubmit: PropTypes.func.isRequired,
-}
+AddBugModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
